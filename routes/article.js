7463f5f03cd6c6bf88fab8be775909698dda0a6b
enkronan <robin@tempura.se>
2020-08-12 22:23:24 +0200

    dev
    added routes for articles

--- routes/article.js
+++ routes/article.js
@@ -6,3 +6,63 @@ exports.show = (req, res, next) => {
         res.render('article', article)
     })
 }
+
+exports.list = (req, res, next) => {
+    req.collections.articles.find({}).toArray((error, articles) => {
+        if (error) return next(error)
+        res.send({articles: articles})
+    })
+}
+
+exports.add = (req, res, next) => {
+    if (!req.body.article) return next(new Error("No article payload."))
+    let article = req.body.article
+    article.published =  false
+    req.collections.articles.insert(article, (error, articleResponse) => {
+        if (error) return next(error)
+        res.send(articleResponse)
+    })
+}
+
+exports.edit = (req, res, next) => {
+    if (!req.param.id) return next(new Error('No article id.'))
+    req.collections.articles.updateById(req.params.id, {$set: req.body.article}, (error, count) => {
+        if (error) return next(error)
+        res.send({affectedCount: count})
+    })
+}
+
+exports.del = (req, res, next) => {
+    if (!req.params.id) return next(new Error('No article ID.'))
+    req.collections.articles.removeById(req.params.id, (error, count) => {
+        if (error) return next(error)
+        res.send({affectedCount: count})
+    })
+}
+
+exports.post = (req, res, next) => {
+    if (!req.body.title) { res.render('post') }
+}
+
+exports.postArticle = (req, res, next) => {
+    if (!req.body.title || !req.body.slug || !req.body.text) {
+        return res.render('post', {error: 'Fill title, slug and text.'})
+    }
+    const article = {
+        title: req.body.title,
+        slug: req.body.slug,
+        text: req.body.text,
+        published: false
+    }
+    req.collections.articles.insert(article, (error, articleResponse) => {
+        if (error) return next(error)
+        res.render('post', {error: "Article was added. Publish it on admin page"})
+    })
+}
+
+exports.admin = (req, res, next) => {
+    req.collections.articles.find({}, {sort: {_id:-1}}).toArray((error, articles) => {
+        if (error) return next(error)
+        res.render('admin', {articles: articles})
+    })
+}
