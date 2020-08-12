6c3dc3e3611e8971acf13a541d2b3f470fffd460
enkronan <robin@tempura.se>
2020-08-12 22:05:12 +0200

    dev
    added user routes

---
+++ routes/user.js
@@ -0,0 +1,15 @@
+exports.list = (req, res, next) => {
+    res.send('respond with a resource')
+}
+
+exports.login = (req, res, next) => {
+    res.render('login')
+}
+
+exports.logout = (req, res, next) => {
+    res.redirect('/')
+}
+
+exports.authenticate = (req, res, next) => {
+    res.redirect('/admin')
+}
