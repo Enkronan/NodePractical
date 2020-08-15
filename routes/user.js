f0902b086df6c6f858165a948f880f9497f8d47b
enkronan <robin@tempura.se>
2020-08-15 18:05:16 +0200

    dev
    added authentication routes

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,23 @@ exports.login = (req, res, next) => {
 }
 
 exports.logout = (req, res, next) => {
+    req.session.destroy()
     res.redirect('/')
 }
 
 exports.authenticate = (req, res, next) => {
-    res.redirect('/admin')
+    if (!req.body.email || !req.body.password) {
+        return res.render('login', {error: "Please provide email and password"})
+    }
+    req.collections.users.findOne({
+        email: req.body.email,
+        password: req.body.password
+    }, (error, user) => {
+        if (error) return next(error)
+        if (!user) return res.render('login', {error: "Incorrect email and password combination"})
+        //successfull sign-in
+        req.session.user = user
+        req.session.admin = user.admin
+        res.redirect('/admin')
+    })
 }
